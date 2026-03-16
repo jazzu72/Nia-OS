04d28e0bf57b28e40ee5914e4a3f081fd044ed6b
jazzu72 <lesane1972@gmail.com>
2026-03-16 05:37:27 -0400

    Create map.ts

---
+++ hub/engine/main/interface/interpreter/map.ts
@@ -0,0 +1,10 @@
+{
+  type: "create_automation",
+  ruleType: "threshold",
+  config: {
+    threshold: 20000,
+    amount: 500,
+    fromWallet: "founder_wallet",
+    targetWallet: "treasury_wallet"
+  }
+}
